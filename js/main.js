1016a4594db5ca732e4f0fa8e60b6e0eb77db767
NehalAltantawy <71020588+NehalAltantawy@users.noreply.github.com>
2022-04-23 13:15:22 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const N = document.getElementById('new');
+const C = document.getElementById('creat')
+
+
+function showForm(){
+    N.style.display ="block";
+    C.style.display = "none";
+}
+
+function loginAcount(){
+    N.style.display ="none";
+    C.style.display = "block";
+}
